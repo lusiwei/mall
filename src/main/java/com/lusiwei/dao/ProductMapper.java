af3b14732696b605ca738b017576a4803dc8b301
lusiwei <951571596@qq.com>
2018-11-23 23:38:46 +0800

    ssm 整合，第一次提交

---
+++ src/main/java/com/lusiwei/dao/ProductMapper.java
@@ -0,0 +1,20 @@
+package com.lusiwei.dao;
+
+import com.lusiwei.pojo.Product;
+import com.lusiwei.pojo.ProductWithBLOBs;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ProductWithBLOBs record);
+
+    int insertSelective(ProductWithBLOBs record);
+
+    ProductWithBLOBs selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ProductWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(ProductWithBLOBs record);
+
+    int updateByPrimaryKey(Product record);
+}
---
+++ src/main/java/com/lusiwei/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lusiwei.dao;
+
+import com.lusiwei.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
